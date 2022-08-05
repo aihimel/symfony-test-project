2f1afeb2fd2ef83526dc6d2885c5a19d2be7d8e0
Aftabul Islam <toaihimel@gmail.com>
2022-08-05 16:56:34 +0600

    Initial Commit

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use strict;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class LuckyController 
+{
+
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+        return new Response(
+            "
+            <html>
+                <body>
+                    Lucky Number: {$number}
+                </body>
+            </html>
+            "
+        );
+    }
+
+}
